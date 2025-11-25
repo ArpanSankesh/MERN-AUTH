16e933486b91d9907c7f21f1fd7ebf063bffc744
ArpanSankesh <arpansankesh830@gmail.com>
2025-11-25 22:52:56 +0530

    Auth Controller: Logout Controller removes the Cookes (clearCookie) this is log out the user

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -2,7 +2,7 @@ import bcrpty from "bcrypt";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
-//REGISTER USER
+//----------------------------REGISTER USER----------------------------
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -31,13 +31,13 @@ export const register = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.json({ success: true });
+    return res.json({ success: true, message:'User Created'});
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
 };
 
-// Login User
+// ----------------------------Login User----------------------------
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
@@ -71,8 +71,23 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.json({ success: true });
-  } catch (error) {
+    return res.json({ success: true, message:'Login Successfull'});
+} catch (error) {
     return res.json({ success: false, message: error.message });
-  }
+}
 };
+
+//----------------------------Logout----------------------------
+
+export const logout = async (req, res) => {
+    try {
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV == "production" ? "none" : "strict",
+    })
+    return res.json({ success: true, message:'User LOGGED OUT'});
+ } catch (error) {
+    return res.json({ success: false, message: error.message });
+ }
+}
