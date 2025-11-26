420b759da5d412ebe29750018ab64a31ec306e53
ArpanSankesh <arpansankesh830@gmail.com>
2025-11-26 20:24:58 +0530

    Created a user Controller which finds the user Id and then return user name and if it is verified or not

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 
 import authRouter from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,5 +20,6 @@ app.get('/', (req, res) =>
     res.send(`API WORKING !!!`)
 )
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
  
 app.listen(port, () => console.log(`Server Running on PORT : ${port}`))
