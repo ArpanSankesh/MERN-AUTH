ffd8d62c8015a0ff8e0cefe74e5a31ac4cffd101
ArpanSankesh <arpansankesh830@gmail.com>
2025-11-26 23:20:00 +0530

    Login Ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+        <NavBar/>
+        <Header/>
+    </div>
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
+  const [state, setState] = useState('Sign Up')
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+      <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 sm:w-32 cursor-pointer' />
+      <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
+
+        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create account' : 'Login '}</h2>
+        <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login in to your account'}</p>
+
+        <form>
+          {state === 'Sign Up' && (
+            <div className='mb-4 flex items-center gap-3 w-full px-7 py-3 rounded-full bg-[#333A5C]'>
+              <img src={assets.person_icon} alt="" />
+              <input className='bg-transparent outline-none' type="text" placeholder='Full Name' required />
+            </div>
+          )}
+
+          <div className='mb-4 flex items-center gap-3 w-full px-7 py-3 rounded-full bg-[#333A5C]'>
+            <img src={assets.mail_icon} alt="" />
+            <input className='bg-transparent outline-none' type="email" placeholder='Email ID' required />
+          </div>
+          <div className='mb-4 flex items-center gap-3 w-full px-7 py-3 rounded-full bg-[#333A5C]'>
+            <img src={assets.lock_icon} alt="" />
+            <input className='bg-transparent outline-none' type="password" placeholder='Password' required />
+          </div>
+
+          {state === 'Login' && (<p className='mb-4 text-indigo-500 cursor-pointer'>Forgot Password</p>)}
+          
+          <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium'>{state}</button>
+        </form>
+        {state === 'Sign Up' ? (
+          <p onClick={()=> setState('Login')} className='text-gray-400 text-center text-xs mt-4'>already have an account?{' '} <span className='text-blue-400 cursor-pointer underline'>Login here</span></p>
+        ) : (
+          <p onClick={()=> setState('Sign Up')} className='text-gray-400 text-center text-xs mt-4'>Don't have an account?{' '} <span className='text-blue-400 cursor-pointer underline'>Sign Up</span></p>
+        )}
+
+
+      </div>
+    </div>
   )
 }
 
