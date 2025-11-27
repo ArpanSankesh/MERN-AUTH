3508441382af9cd630dd25dfd125e3e66b376c00
ArpanSankesh <arpansankesh830@gmail.com>
2025-11-27 20:14:21 +0530

    Added the complete login and logout funcnality

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,26 +1,45 @@
 import axios  from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+    const navigate = useNavigate()
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userData, setUserData] = useState(false);
+    
+    axios.defaults.withCredentials = true;
+
+
+    const getAuthState = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + ('/api/auth/is-auth'))
+            if(data.success){
+                setIsLoggedIn(true)
+                getUserData()
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const getUserData = async () => {
         try {
             const {data} = await axios.get(backendUrl + ('/api/user/data'))
             data.success ? setUserData(data.userData) : toast.success(data.message)
         } catch (error) {
-            toast.success(error.message)
+            toast.error(error.message)
         }
     }
     
-    
+    useEffect(()=>{
+        getAuthState();
+    },[])
 
     const value = {
         backendUrl,
--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets';
 
 const EmailVerify = () => {
+  const navigate = useNavigate();
+  
   return (
-    <div>EmailVerify</div>
+    <div className='flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400'>
+      <img onClick={() => navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 sm:w-32 cursor-pointer' />
+    </div>
   )
 }
 
